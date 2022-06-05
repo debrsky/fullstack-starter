fe238899755cd0fc6b7dac85227d227528e6d574
debrsky <debrsky@gmail.com>
2022-06-05 20:25:30 +1000

    add replacing saved password with hash

--- server/lib/users.js
+++ server/lib/users.js
@@ -1,22 +1,77 @@
 const config = require('../../config.js');
 const fs = require('fs').promises;
+const writeFileAtomic = require('write-file-atomic');
 const path = require('path');
+const {pbkdf2, randomBytes} = require('crypto');
+
+const ITERATIONS = 100000;
+const KEYLEN = 32;
+const DIGEST = 'sha512';
+const ENCODING = 'base64url';
 
 const usersFile = path.join(config.rootDir, config.storage, 'users.json');
 
 async function getUserById(id) {
-	const users = JSON.parse(await fs.readFile(usersFile, 'utf-8'));
+	const users = await readUsers();
 	return users.find((user) => user.id === id);
 }
 
 async function getUserByNamePassword(username, password) {
-	const users = JSON.parse(await fs.readFile(usersFile, 'utf-8'));
-	const user = users.find((user) => user.name === username);
+	const usernameNormalized = username.normalize();
+
+	const users = await readUsers();
+	const user = users.find(
+		(user) => user.name.normalize() === usernameNormalized
+	);
 	if (!user) return null;
 
-	if (!user.password === password) return null;
+	const derivedKey = await pbkdf2Async(
+		password.normalize(),
+		user.salt,
+		ITERATIONS,
+		KEYLEN,
+		DIGEST
+	);
+	if (!user.derivedKey === derivedKey) return null;
 
 	return user;
+}
+
+function pbkdf2Async(password, salt, iterations, keylen, digest) {
+	const executor = (resolve, reject) => {
+		pbkdf2(password, salt, iterations, keylen, digest, (err, derivedKey) => {
+			if (err) return reject(err);
+			resolve(derivedKey.toString(ENCODING));
+		});
+	};
+
+	return new Promise(executor);
+}
+
+async function readUsers() {
+	const users = JSON.parse(await fs.readFile(usersFile, 'utf-8'));
+	if (!users.some((user) => Boolean(user.password))) return users;
+
+	for (const user of users) {
+		if (!user.password) continue;
+
+		const secret = user.password.normalize();
+		const salt = randomBytes(KEYLEN).toString(ENCODING);
+
+		user.derivedKey = await pbkdf2Async(
+			secret,
+			salt,
+			ITERATIONS,
+			KEYLEN,
+			DIGEST
+		);
+		user.salt = salt;
+		delete user.password;
+	}
+
+	await writeFileAtomic(usersFile, JSON.stringify(users, null, 4));
+
+	return users;
 }
 
 module.exports = {getUserById, getUserByNamePassword};
