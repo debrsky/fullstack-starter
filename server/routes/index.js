754dbb9f4909e0627f2750264b5c5d8c346b13fd
debrsky <debrsky@gmail.com>
2022-06-05 22:54:41 +1000

    fix bug with saving session

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,10 @@ const router = express.Router();
 router.get('/', function (req, res, next) {
 	const counter = (req.session.counter ?? 0) + 1;
 	req.session.counter = counter;
-	res.render('index', {title: 'Express', counter});
+	req.session.save((err) => {
+		if (err) return next(err);
+		res.render('index', {title: 'Express', counter});
+	});
 });
 
 module.exports = router;
