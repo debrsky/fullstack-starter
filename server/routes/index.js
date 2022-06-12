55327522299b83f40f43940b9972cd7707ddd333
debrsky <debrsky@gmail.com>
2022-06-12 12:06:54 +1000

    add logout link to index.pug

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	const counter = (req.session.counter ?? 0) + 1;
+	const user = req.user;
+
 	req.session.counter = counter;
 	req.session.save((err) => {
 		if (err) return next(err);
-		res.render('index', {title: 'Express', counter});
+		res.render('index', {title: 'Express', counter, user});
 	});
 });
 
