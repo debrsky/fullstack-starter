0984e8ee98d10de22fcee0f47d93cd2e6cbe4a76
debrsky <debrsky@gmail.com>
2022-10-04 14:44:59 +1000

    add error message on intenral server error

--- server/app.js
+++ server/app.js
@@ -56,6 +56,9 @@ app.use(function (err, req, res, next) {
 
 	// render the error page
 	res.status(err.status || 500);
+
+	if (req.xhr) return res.send(err.message);
+
 	res.render('error');
 });
 
